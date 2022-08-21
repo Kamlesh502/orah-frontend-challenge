b42ce6abd3707ff46693c4e9dfbfd82fc6fa4700
Kamlesh Bishnoi <kamleshbishnoi502@gmail.com>
2022-08-22 01:57:50 +0530

    Task 1 done

--- front-end/src/staff-app/daily-care/home-board.page.tsx
+++ front-end/src/staff-app/daily-care/home-board.page.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 import Button from "@material-ui/core/ButtonBase"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSort } from "@fortawesome/free-solid-svg-icons"
 import { Spacing, BorderRadius, FontWeight } from "shared/styles/styles"
 import { Colors } from "shared/styles/colors"
 import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
@@ -13,14 +14,47 @@ import { ActiveRollOverlay, ActiveRollAction } from "staff-app/components/active
 export const HomeBoardPage: React.FC = () => {
   const [isRollMode, setIsRollMode] = useState(false)
   const [getStudents, data, loadState] = useApi<{ students: Person[] }>({ url: "get-homeboard-students" })
-
+  const [firstNameSortType, setFirstNameSortType] = useState("asc")
+  const [lastNameSortType, setLastNameSortType] = useState("asc")
+  const [inputStudent, setInputStudent] = useState("")
+  const [filterStudentMode, setFilterStudentMode] = useState(false)
+  const [filteredStudentData, setFilteredStudentData] = useState<Person[] | undefined>([])
   useEffect(() => {
     void getStudents()
+    setInputStudent("")
+    setFilterStudentMode(false)
   }, [getStudents])
 
   const onToolbarAction = (action: ToolbarAction) => {
     if (action === "roll") {
       setIsRollMode(true)
+    }
+    if (action === "sort_first_name") {
+      if (firstNameSortType === "asc") {
+        filterStudentMode ? filteredStudentData?.sort((a, b) => a.first_name.localeCompare(b.first_name)) : data?.students.sort((a, b) => a.first_name.localeCompare(b.first_name))
+        setFirstNameSortType(firstNameSortType === "asc" ? "desc" : "asc")
+      } else {
+        filterStudentMode ? filteredStudentData?.sort((a, b) => b.first_name.localeCompare(a.first_name)) : data?.students.sort((a, b) => b.first_name.localeCompare(a.first_name))
+        setFirstNameSortType(firstNameSortType === "asc" ? "desc" : "asc")
+      }
+    }
+    if (action === "sort_last_name") {
+      if (lastNameSortType === "asc") {
+        filterStudentMode ? filteredStudentData?.sort((a, b) => a.last_name.localeCompare(b.last_name)) : data?.students.sort((a, b) => a.last_name.localeCompare(b.last_name))
+        setLastNameSortType(lastNameSortType === "asc" ? "desc" : "asc")
+      } else {
+        filterStudentMode ? filteredStudentData?.sort((a, b) => b.last_name.localeCompare(a.last_name)) : data?.students.sort((a, b) => b.last_name.localeCompare(a.last_name))
+        setLastNameSortType(lastNameSortType === "asc" ? "desc" : "asc")
+      }
+    }
+    if (action === "search_by_name") {
+      if (inputStudent === "") {
+        setFilterStudentMode(false)
+      }
+      const filteredStudent = data?.students.filter(
+        (student) => student.first_name.toLowerCase().includes(inputStudent.toLowerCase()) || student.last_name.toLowerCase().includes(inputStudent.toLowerCase())
+      )
+      setFilteredStudentData(filteredStudent)
     }
   }
 
@@ -33,7 +67,7 @@ export const HomeBoardPage: React.FC = () => {
   return (
     <>
       <S.PageContainer>
-        <Toolbar onItemClick={onToolbarAction} />
+        <Toolbar onItemClick={onToolbarAction} setInputStudent={setInputStudent} setFilterStudentMode={setFilterStudentMode} />
 
         {loadState === "loading" && (
           <CenteredContainer>
@@ -43,9 +77,9 @@ export const HomeBoardPage: React.FC = () => {
 
         {loadState === "loaded" && data?.students && (
           <>
-            {data.students.map((s) => (
-              <StudentListTile key={s.id} isRollMode={isRollMode} student={s} />
-            ))}
+            {filterStudentMode
+              ? filteredStudentData?.map((student) => <StudentListTile student={student} key={student.id} isRollMode={isRollMode} />)
+              : data?.students.map((student) => <StudentListTile student={student} key={student.id} isRollMode={isRollMode} />)}
           </>
         )}
 
@@ -60,16 +94,41 @@ export const HomeBoardPage: React.FC = () => {
   )
 }
 
-type ToolbarAction = "roll" | "sort"
+type ToolbarAction = "roll" | "sort_first_name" | "sort_last_name" | "search_by_name"
 interface ToolbarProps {
   onItemClick: (action: ToolbarAction, value?: string) => void
+  setInputStudent: (value: string) => void
+  setFilterStudentMode: (value: boolean) => void
 }
 const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { onItemClick } = props
+  const { onItemClick, setInputStudent, setFilterStudentMode } = props
   return (
     <S.ToolbarContainer>
-      <div onClick={() => onItemClick("sort")}>First Name</div>
-      <div>Search</div>
+      <div onClick={() => onItemClick("sort_first_name")} style={{ cursor: "pointer" }}>
+        First Name <FontAwesomeIcon icon={faSort} />
+      </div>
+      <div onClick={() => onItemClick("sort_last_name")} style={{ cursor: "pointer" }}>
+        Last Name <FontAwesomeIcon icon={faSort} />
+      </div>
+      <div>
+        <input
+          onChange={(e) => setInputStudent(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              setFilterStudentMode(true)
+              onItemClick("search_by_name")
+            }
+          }}
+        />
+        <button
+          onClick={() => {
+            setFilterStudentMode(true)
+            onItemClick("search_by_name")
+          }}
+        >
+          Search
+        </button>
+      </div>
       <S.Button onClick={() => onItemClick("roll")}>Start Roll</S.Button>
     </S.ToolbarContainer>
   )
